dfdfd07e4275b71a3f3fbbf42f8b5bd5aacf8e0c
tsinghsisodia <75780413+tsinghsisodia@users.noreply.github.com>
2021-05-23 12:44:09 +0530

    Update test_name_function.py

--- test_name_function.py
+++ test_name_function.py
@@ -1,7 +1,6 @@
 import unittest
 from name_function import formatted_name
 
-
 class MyTestCase(unittest.TestCase):
 
     def test_first_last_name(self):
@@ -11,7 +10,6 @@ class MyTestCase(unittest.TestCase):
     def test_first_last_middle_name(self):
         result = formatted_name("Tarun", "Sisodia", "Singh")
         self.assertEqual(result, "Tarun Singh Sisodia")
-
 
 if __name__ == '__main__':
     unittest.main()
